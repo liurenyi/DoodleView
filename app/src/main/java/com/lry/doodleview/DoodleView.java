300b9a43254a17dd6d7fed6ae73405db0d50f419
liurenyi <928171620@qq.com>
2017-08-22 17:45:00 +0800

    增加返回撤销，改变颜色，改变画笔宽度功能。

--- app/src/main/java/com/lry/doodleview/DoodleView.java
+++ app/src/main/java/com/lry/doodleview/DoodleView.java
@@ -1,9 +1,11 @@
 package com.lry.doodleview;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -24,29 +26,43 @@ public class DoodleView extends View {
     private float mDownX, mDownY;
     private float mTempX, mTempY;
     private List<DrawPath> drawPathList;
-
+    private List<DrawPath> cacheDrawPathList;
     public DoodleView(Context context) {
         super(context);
-        Log.d("liu","111");
+        Log.d("liu", "111");
     }
 
     public DoodleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        Log.d("liu","222");
+        Log.d("liu", "222");
         drawPathList = new ArrayList<>();
+        cacheDrawPathList = new ArrayList<>();
         initPaint();
     }
 
     public DoodleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Log.d("liu","333");
+        Log.d("liu", "333");
 
     }
 
     private void initPaint() {
+        SharedPreferences share = PreferenceManager.getDefaultSharedPreferences(getContext());
+        int color = share.getInt("colorType", 0);
+        float paintWidth = share.getFloat("paintWidth", 0);
         mPaint = new Paint();
+
+        if (color != 0) {
+            mPaint.setColor(color);
+        }
+
+        if (paintWidth != 0) {
+            mPaint.setStrokeWidth(paintWidth);
+        } else {
+            mPaint.setStrokeWidth(5);
+        }
+
         mPaint.setAntiAlias(true);
-        mPaint.setStrokeWidth(5);
         mPaint.setStyle(Paint.Style.STROKE);
     }
 
@@ -56,7 +72,7 @@ public class DoodleView extends View {
         if (drawPathList != null && !drawPathList.isEmpty()) {
             for (DrawPath drawPath : drawPathList) {
                 if (drawPath.path != null) {
-                    canvas.drawPath(drawPath.path,drawPath.paint);
+                    canvas.drawPath(drawPath.path, drawPath.paint);
                 }
             }
         }
@@ -95,8 +111,39 @@ public class DoodleView extends View {
 
     public void undo() {
         if (drawPathList != null && drawPathList.size() >= 1) {
+            cacheDrawPathList.add(drawPathList.get(drawPathList.size() - 1)); // 先把要移除的笔画保存起来
             drawPathList.remove(drawPathList.size() - 1);
             invalidate();
         }
+    }
+
+    public void backUndo() {
+        if (cacheDrawPathList != null && cacheDrawPathList.size() >= 1) {
+            drawPathList.add(cacheDrawPathList.get(cacheDrawPathList.size() - 1)); // 先把要增加的笔画保存起来
+            cacheDrawPathList.remove(cacheDrawPathList.size() - 1);
+            invalidate();
+        }
+    }
+
+    /**
+     * 添加改变画笔颜色方法
+     * @param color
+     */
+    public void resetColor(int color) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putInt("colorType",color);
+        editor.apply(); // 保存当前画笔颜色状态
+        mPaint.setColor(color);
+    }
+
+    /**
+     * 改变画笔宽度
+     * @param paintWidth
+     */
+    public void resetPaintWidth(float paintWidth) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        editor.putFloat("paintWidth",paintWidth);
+        editor.apply(); // 保存当前画笔宽度
+        mPaint.setStrokeWidth(paintWidth);
     }
 }
--- app/src/main/java/com/lry/doodleview/DoodleViewActivity.java
+++ app/src/main/java/com/lry/doodleview/DoodleViewActivity.java
@@ -1,15 +1,22 @@
 package com.lry.doodleview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * http://blog.csdn.net/coderinchina/article/details/53690218
+ */
+
 public class DoodleViewActivity extends AppCompatActivity implements View.OnClickListener{
 
     private DoodleView doodleView;
     private Button undo;
-
+    private Button backUndo;
+    private Button resetRedColor;
+    private Button resetPaintWidth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +26,18 @@ public class DoodleViewActivity extends AppCompatActivity implements View.OnClic
 
     private void init() {
         doodleView = (DoodleView) findViewById(R.id.doodle_view);
+
         undo = (Button) findViewById(R.id.undo);
         undo.setOnClickListener(this);
+
+        backUndo = (Button) findViewById(R.id.back_undo);
+        backUndo.setOnClickListener(this);
+
+        resetRedColor = (Button) findViewById(R.id.reset_color);
+        resetRedColor.setOnClickListener(this);
+
+        resetPaintWidth = (Button) findViewById(R.id.reset_paint_width);
+        resetPaintWidth.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +45,15 @@ public class DoodleViewActivity extends AppCompatActivity implements View.OnClic
         switch (v.getId()) {
             case R.id.undo:
                 doodleView.undo();
+                break;
+            case R.id.back_undo:
+                doodleView.backUndo();
+                break;
+            case R.id.reset_color:
+                doodleView.resetColor(Color.RED);
+                break;
+            case R.id.reset_paint_width:
+                doodleView.resetPaintWidth(10);
                 break;
         }
     }
